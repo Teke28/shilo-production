96f43f811f47c60f83a224f1bb489e8c6440423a
Tekele Tesfeye <tasfeyetakele@gmail.com>
2026-08-25 11:27:03 +0300

    Add Amharic language file

---
+++ languages/am.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * Shilo Production - Amharic Language File
+ */
+
+$t = [
+    // Navigation
+    'nav_home' => 'ዋና ገጽ',
+    'nav_about' => 'ስለ እኛ',
+    'nav_services' => 'ሴቪሶች',
+    'nav_portfolio' => 'ስራዎች',
+    'nav_videos' => 'ቪዲዮዎች',
+    'nav_blog' => 'ብሎግ',
+    'nav_contact' => 'ተገኝ',
+    'nav_book' => 'አሁን ያዘዙ',
+    'nav_login' => 'አስተዳዳሪ መግቢያ',
+    'nav_logout' => 'ውጃ',
+    
+    // Hero Section
+    'hero_title' => 'ታሪኮችን እናያዝላለን። ገጠር እናፍጠራለን።',
+    'hero_subtitle' => 'ለ ክስተቶች፣ ንግዶች፣ ሰዎች እና የማይረሳ ፍንጮች ሙያዊ ፎቶግራፊ፣ ቪዲዮግራፊ እና ጎበዝ ሚዲያ ምርት።',
+    'hero_explore' => 'ስራዎችን ይተላለፉ',
+    'hero_book' => 'ፋይናል ያዘዙ',
+    
+    // Home Page
+    'home_about_preview' => 'ስለ እኛ',
+    'home_services' => 'ሴቪሶቻችን',
+    'home_portfolio' => 'ታዋቅ ስራዎች',
+    'home_videos' => 'ታዋቅ ቪዲዮዎች',
+    'home_why_us' => 'ለምን እኛን ይመርጡ',
+    'home_testimonials' => 'የደንበኛ ምስክርነቶች',
+    'home_blog' => 'የመጨረሻ ዜናዎች',
+    'home_cta' => 'አስደናቂ ነገር መፍጠር ወደ ዝግጅት ጨርሰዋል?',
+    'home_cta_text' => 'የእርስዎ ራዕይ ወደ ሕይወት ለማምጣት ሙያዊ ሚዲያ ምርት ስሪት።',
+    
+    // About Page
+    'about_title' => 'ስሊሞ ፍቅረስ',
+    'about_mission' => 'የእኛ ሚሽን',
+    'about_mission_text' => 'ፋሊ ታሪኮችን ለመግለጥ እና ከደንበኞች ጠበቃዎች ያለፉ ሚዲያ ምርት ሴቪስ ማቅረብ።',
+    'about_vision' => 'የእኛ ራዕይ',
+    'about_vision_text' => 'ለ ንግድ ሚዲያ ምርት ኩባንያ ከፍተኛ ተወቅቃ ሙያ እና ደንበኛ 족족 ተመርጠን።',
+    'about_team' => 'የእኛ ቡድን',
+    
+    // Services Page
+    'services_title' => 'ሴቪሶቻችን',
+    'services_description' => 'ወሰዓ ሚዲያ ምርት ውሳኔዎች ደንበኛ ሙቀት ለግለሰበኛ',
+    'service_starting_price' => 'ከ ጀምር',
+    'service_duration' => 'ጊዜ',
+    'service_features' => 'ባህሪዎች',
+    'service_view_details' => 'ዝርዝር ይተላለፉ',
+    'service_book_now' => 'አሁን ያዘዙ',
+    
+    // Portfolio Page
+    'portfolio_title' => 'ስራዎቻችን',
+    'portfolio_filter_all' => 'ሁሉ ስራዎች',
+    'portfolio_search' => 'ስራ ይፈልጉ...',
+    'portfolio_view_project' => 'ስራ ይተላለፉ',
+    'portfolio_view_gallery' => 'ምስሎች ይተላለፉ',
+    
+    // Videos Page
+    'videos_title' => 'ቪዲዮዎቻችን',
+    'videos_description' => 'የመጨረሻ ቪዲዮ ምርቶችን ይተላለፉ',
+    'videos_watch' => 'ቪዲዮ ይተላለፉ',
+    
+    // Booking Page
+    'booking_title' => 'ፋይናል ጠይቅ',
+    'booking_description' => 'ስር ፎርም ይሙሉ ፋይናል መሙያ ጠይቅ',
+    'booking_full_name' => 'ሙሉ ስም',
+    'booking_company' => 'ኩባንያ',
+    'booking_email' => 'ኢሜይል አድራሻ',
+    'booking_phone' => 'ስልክ ቁጥር',
+    'booking_service' => 'ሴቪስ ይምረጡ',
+    'booking_project_type' => 'ስራ አይነት',
+    'booking_event_date' => 'ክስተት ቀን',
+    'booking_location' => 'ቦታ',
+    'booking_participants' => 'የተሳታፊዎች ቁጥር',
+    'booking_budget' => 'በጀት ወሰን',
+    'booking_description' => 'ስራ ገለጻ',
+    'booking_requirements' => 'ተጨማሪ ስጋቶች',
+    'booking_submit' => 'ፋይናል ጠይቅ ከክምር',
+    'booking_success' => 'ፋይናል ጠይቅ በተሳካ ምቱ ተከክሎ',
+    'booking_reference' => 'ፋይናል ምልክት',
+    
+    // Quote Page
+    'quote_title' => 'ዋጋ ጠይቅ',
+    'quote_description' => 'ለ ስራ ሙኩአዊ ዋጋ ይቅር',
+    'quote_submit' => 'ዋጋ ጠይቅ',
+    'quote_success' => 'ዋጋ ጠይቅ በተሳካ ተከክሎ',
+    
+    // Contact Page
+    'contact_title' => 'እኛን ተገኙ',
+    'contact_form_title' => 'መልዕክት ይላኩ',
+    'contact_name' => 'የእርስዎ ስም',
+    'contact_email' => 'የእርስዎ ኢሜይል',
+    'contact_phone' => 'የእርስዎ ስልክ',
+    'contact_subject' => 'ርዕስ',
+    'contact_message' => 'መልዕክት',
+    'contact_send' => 'መልዕክት ይላኩ',
+    'contact_phone_label' => 'ስልክ',
+    'contact_email_label' => 'ኢሜይል',
+    'contact_address' => 'አድራሻ',
+    'contact_hours' => 'ስራ ሰዓት',
+    'contact_success' => 'መልዕክት በተሳካ ተላኩ',
+    
+    // Testimonials Page
+    'testimonials_title' => 'የደንበኛ ምስክርነቶች',
+    'testimonials_description' => 'ደንበኞች ስለ እኛ ምን ይላሉ',
+    'testimonials_submit_title' => 'ገጠር ጋር ያስጋ',
+    'testimonials_submit' => 'ምስክርነት ከክምር',
+    
+    // Blog Page
+    'blog_title' => 'የመጨረሻ ዜናዎች እና ታሪኮች',
+    'blog_read_more' => 'ተጨማሪ ያንብቡ',
+    'blog_published' => 'ታተመ',
+    'blog_by' => 'በ',
+    'blog_category' => 'ምድብ',
+    
+    // Footer
+    'footer_services' => 'ሴቪሶች',
+    'footer_photography' => 'ፎቶግራፊ',
+    'footer_videography' => 'ቪዲዮግራፊ',
+    'footer_production' => 'ሚዲያ ምርት',
+    'footer_creative' => 'ጎበዝ ሴቪሶች',
+    'footer_quick_links' => 'ፈጣን ሊንኮች',
+    'footer_contact' => 'ተገኝ',
+    'footer_social' => 'እኛን ይከተሉ',
+    'footer_copyright' => '© 2026 ሞሎ ፍቅረስ። ሁሉ መብቶች የተጠበቁ።',
+    
+    // Form Validation
+    'error_required_field' => 'ይህ መስክ ተበጀ ነው',
+    'error_invalid_email' => 'እባክወ ትክክለኛ ኢሜይል አድራሻ ያስገቡ',
+    'error_invalid_phone' => 'እባክወ ትክክለኛ ስልክ ቁጥር ያስገቡ',
+    'error_invalid_date' => 'እባክወ ትክክለኛ ቀን ያስገቡ',
+    'error_future_date' => 'ክስተት ቀን ወደፊት መሆን አለበት',
+    'error_file_upload' => 'ፋይል መሙያ ወድቅ ሆነ',
+    'error_csrf' => 'ደህንነት ረጋገጥ ወድቅ ሆነ',
+    
+    // Admin Panel
+    'admin_title' => 'አስተዳዳሪ ዳሳሽ',
+    'admin_login' => 'አስተዳዳሪ መግቢያ',
+    'admin_email' => 'ኢሜይል',
+    'admin_password' => 'የይለፍ ሃረግ',
+    'admin_remember' => 'ታስብ',
+    'admin_login_button' => 'ቢ',
+    'admin_dashboard' => 'ዳሳሽ',
+    'admin_portfolio' => 'ስራዎች',
+    'admin_videos' => 'ቪዲዮዎች',
+    'admin_services' => 'ሴቪሶች',
+    'admin_bookings' => 'ፋይናል',
+    'admin_quotes' => 'ዋጋዎች',
+    'admin_messages' => 'መልዕክቶች',
+    'admin_testimonials' => 'ምስክርነቶች',
+    'admin_blog' => 'ብሎግ',
+    'admin_team' => 'ቡድን',
+    'admin_media' => 'ሚዲያ',
+    'admin_settings' => 'ቅንጅቶች',
+    'admin_logout' => 'ውጃ',
+    
+    // Admin Actions
+    'admin_add' => 'አዲስ ያክሉ',
+    'admin_edit' => 'ያርም',
+    'admin_delete' => 'ይሂዱ',
+    'admin_view' => 'ይተላለፉ',
+    'admin_save' => 'አስቀምጥ',
+    'admin_cancel' => 'ይህ አታደርግ',
+    'admin_confirm_delete' => 'ይህ ነገር ውጃ ከብዛትዎ ወይ?',
+    'admin_success' => 'ስራ በተሳካ ተከክሎ',
+    'admin_error' => 'ስህተት ተሰራ',
+    
+    // Status Labels
+    'status_pending' => 'የሚጠብቅ',
+    'status_approved' => 'ተጋዝዞ',
+    'status_rejected' => 'ውድቀት',
+    'status_published' => 'ታተመ',
+    'status_draft' => 'ረቂቅ',
+    'status_active' => 'ንቁ',
+    'status_inactive' => 'ግልጥ',
+    'status_completed' => 'የተቋረጠ',
+    'status_confirmed' => 'ተጋዝዞ',
+];
